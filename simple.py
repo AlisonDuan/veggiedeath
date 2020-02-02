af4a179866de206c160cfb1ec9126009250605cd
Alison Duan <aduan@dhcp-rhodes-2915.redrover.cornell.edu>
2020-02-01 20:22:55 -0500

    added image assignments for vars

--- simple.py
+++ simple.py
@@ -10,9 +10,11 @@ camera = PiCamera()
 camera.start_preview()
 
 for i in range(50):
-    img1 = camera.capture("image1.jpg")
+    camera.capture("image1.jpg")
+    img1 = Image.open("image1.jpg")
     sleep(3)
-    img2 = camera.capture("image2.jpg")
+    camera.capture("image2.jpg")
+    img2 = Image.open("image2.jpg")
     diff = ImageChops.difference(Image.open(img1), Image.open(img2)).histogram()
     rms = math.sqrt(reduce(operator.add, map(lambda diff, i: diff * (i**2), diff, range(256))) / (float(img1.size[0]) * img1.size[1]))
     print("\nDifference: ", rms)
