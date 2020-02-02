acf7e07b45af5951ae83a7d88f998651f6be235b
Alison Duan <aduan@dhcp-rhodes-2915.redrover.cornell.edu>
2020-02-01 20:20:58 -0500

    improved difference with histogram comparison

--- simple.py
+++ simple.py
@@ -13,7 +13,7 @@ for i in range(50):
     img1 = camera.capture("image1.jpg")
     sleep(3)
     img2 = camera.capture("image2.jpg")
-    diff = ImageChops.difference(Image.open(img1), Image.open(img2)).histogram
+    diff = ImageChops.difference(Image.open(img1), Image.open(img2)).histogram()
     rms = math.sqrt(reduce(operator.add, map(lambda diff, i: diff * (i**2), diff, range(256))) / (float(img1.size[0]) * img1.size[1]))
     print("\nDifference: ", rms)
     sleep(3)
