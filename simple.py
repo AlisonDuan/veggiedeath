890532d6db106f79830117e6426bdf96b2740c22
Alison Duan <aduan@dhcp-rhodes-2915.redrover.cornell.edu>
2020-02-01 20:04:22 -0500

    added difference function

--- simple.py
+++ simple.py
@@ -1,7 +1,19 @@
 from picamera import PiCamera
 from time import sleep
+import Image
+import ImageChops
+
 
 camera = PiCamera()
 camera.start_preview()
-sleep(5)
+
+for i in range(50):
+    camera.capture("image1.jpg")
+    sleep(3)
+    camera.capture("image2.jpg")
+    diff = ImageChops.difference(Image.open("image1.jpg"), Image.open("image2.jpg"))
+    print("\nDifference: ", diff)
+    sleep(3)
+
 camera.stop_preview()
+
