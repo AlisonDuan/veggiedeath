4d43afe1b50aca5c0b9cc42e854d02120029a391
Alison Duan <aduan@dhcp-rhodes-2915.redrover.cornell.edu>
2020-02-01 20:19:33 -0500

    improved difference with histogram comparison

--- simple.py
+++ simple.py
@@ -1,3 +1,6 @@
+import math
+from functools import reduce
+import operator
 from picamera import PiCamera
 from time import sleep
 from PIL import Image, ImageChops
@@ -7,11 +10,12 @@ camera = PiCamera()
 camera.start_preview()
 
 for i in range(50):
-    camera.capture("image1.jpg")
+    img1 = camera.capture("image1.jpg")
     sleep(3)
-    camera.capture("image2.jpg")
-    diff = ImageChops.difference(Image.open("image1.jpg"), Image.open("image2.jpg"))
-    print("\nDifference: ", diff)
+    img2 = camera.capture("image2.jpg")
+    diff = ImageChops.difference(Image.open(img1), Image.open(img2)).histogram
+    rms = math.sqrt(reduce(operator.add, map(lambda diff, i: diff * (i**2), diff, range(256))) / (float(img1.size[0]) * img1.size[1]))
+    print("\nDifference: ", rms)
     sleep(3)
 
 camera.stop_preview()
